fbffc430da12fc512375b53ed7cf6110c65e8ede
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-05 13:07:10 +0530

    added path to urls for product detail and checkout

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('checkout/', checkout, name='checkout'),
 ]
