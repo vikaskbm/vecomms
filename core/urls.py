f1f54c316ea66b1be288325bfd0d956b4bf5d434
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-06 00:31:52 +0530

    update urls.py

--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('checkout/', checkout, name='checkout'),
     path('add_to_cart/<slug>/', add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<slug>/', remove_from_cart, name='remove_from_cart'),
 ]
