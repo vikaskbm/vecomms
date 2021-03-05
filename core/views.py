ed96eb22acaf33313d4fe239476792f199071109
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-06 00:29:17 +0530

    Added add_to_cart function that will add items to cart

--- core/views.py
+++ core/views.py
@@ -18,4 +18,33 @@ class ItemDetailView(DetailView):
 
 def checkout(request):
     return render(request, "checkout.html")
- 
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user, 
+        ordered=False
+    )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "Item quantity was updated")
+            return redirect("core:product", slug=slug)
+        else:
+            order.items.add(order_item)
+            messages.info(request, "This item was added to your cart")
+            return redirect("core:product", slug=slug)
+    
+    else:
+        order_date = timezone.now()
+        order = Order.objects.create(user=request.user, order_date=order_date)
+        order.items.add(order_item)
+        messages.info(request, "This item was added to your cart")
+        return redirect("core:product", slug=slug)
+
